eceb629d121a43bc64655e0a58c43c203ac38038
Bram Reuling <bram.reuling@outlook.com>
2021-04-29 16:01:42 +0200

    Created the CameraRig class

---
+++ InternetAdventures/Assets/Scripts/Camera/CameraRig.cs
@@ -0,0 +1,25 @@
+/*
+ * Created By: Bram Reuling
+ */
+
+using UnityEngine;
+
+namespace Camera
+{
+    public class CameraRig : MonoBehaviour
+    {
+        [SerializeField]
+        
+        // Start is called before the first frame update
+        private void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+        
+        }
+    }
+}
