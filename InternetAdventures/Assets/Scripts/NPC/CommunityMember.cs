ff010201cea8a235118848aa08be5faed5fd99b5
Bram Reuling <bram.reuling@outlook.com>
2021-05-11 11:41:12 +0200

    Finished first iteration of chat bubble mech

--- InternetAdventures/Assets/Scripts/NPC/CommunityMember.cs
+++ InternetAdventures/Assets/Scripts/NPC/CommunityMember.cs
@@ -1,15 +1,21 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using UnityEditor;
 using UnityEngine;
 
 namespace NPC
 {
     public class CommunityMember : MonoBehaviour
     {
-        public int minimumLookRange = 10;
+        public float minimumTriggerSpan = 10;
+
+        private float minimumTriggerRadius;
 
         private List<GameObject> players;
         private ChatBubble chatBubble;
+
+        public GameObject cameraRig;
 
         private void Start()
         {
@@ -20,10 +26,34 @@ namespace NPC
         {
             players = GameObject.FindGameObjectsWithTag("Player").ToList();
             chatBubble = GetComponent<ChatBubble>();
+
+            minimumTriggerRadius = minimumTriggerSpan;
         }
 
         private void Update()
         {
+            Vector3 averagePositionBetweenPlayers = players.Aggregate(Vector3.one, (current, player) => current + player.transform.position);
+
+            averagePositionBetweenPlayers /= players.Count;
+
+            float distanceBetweenNpcAndPlayer = Vector3.Distance(transform.position, averagePositionBetweenPlayers);
+
+            if (distanceBetweenNpcAndPlayer <= minimumTriggerRadius)
+            {
+                // Trigger the chat bubble stuffs
+                chatBubble.EnableEmojis();
+            }
+            else
+            {
+                // Disable the chat bubble stuffs
+                chatBubble.DisableEmojis();
+            }
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireSphere(transform.position, minimumTriggerSpan);
         }
     }
 }
