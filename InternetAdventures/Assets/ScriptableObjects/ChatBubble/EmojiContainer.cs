f6d518d7dbb98b287b8776cec337d5715dfd3087
Bram Reuling <bram.reuling@outlook.com>
2021-05-11 09:18:37 +0200

    Created SO for storing emojis

---
+++ InternetAdventures/Assets/ScriptableObjects/ChatBubble/EmojiContainer.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace ScriptableObjects.ChatBubble
+{
+    [CreateAssetMenu(fileName = "New Emoji Container", menuName = "NPC Containers/New Emoji Container", order = 0)]
+    public class EmojiContainer : ScriptableObject
+    {
+        
+    }
+}
--- InternetAdventures/Assets/Scripts/NPC/CommunityMember.cs
+++ InternetAdventures/Assets/Scripts/NPC/CommunityMember.cs
@@ -9,10 +9,17 @@ namespace NPC
         public int minimumLookRange = 10;
 
         private List<GameObject> players;
+        private ChatBubble chatBubble;
 
         private void Start()
         {
+            Init();
+        }
+
+        private void Init()
+        {
             players = GameObject.FindGameObjectsWithTag("Player").ToList();
+            chatBubble = GetComponent<ChatBubble>();
         }
 
         private void Update()
