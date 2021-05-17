a1cbef52b4521a553838239877304535749a9ef0
Bram Reuling <bram.reuling@outlook.com>
2021-05-17 14:42:31 +0200

    Shared dll successfully referenced in server project

---
+++ Server/GameServer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Shared;
+
+namespace Server
+{
+    class GameServer
+    {
+        public static void Main(string[] args)
+        {
+            GameServer server = new GameServer();
+            server.run();
+        }
+
+        private void run()
+        {
+            Log.LogInfo("This is a test", this, ConsoleColor.Red);
+        }
+    }
+}
