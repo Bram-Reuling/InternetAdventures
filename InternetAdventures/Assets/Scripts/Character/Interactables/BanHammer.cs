ceeba032b05885dbdc09e966b2dc36b3fc04c108
Maurice Johannssen <60900273+RulesOfDelta@users.noreply.github.com>
2021-05-17 14:58:35 +0200

    Basic BanHammer implemented

---
+++ InternetAdventures/Assets/Scripts/Character/Interactables/BanHammer.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class BanHammer : Interactable
+{
+    [SerializeField] private LayerMask interactableLayers;
+    private PlayerInput _playerInput;
+    private List<GameObject> _gameObjectsInTrigger = new List<GameObject>();
+
+    private void Start()
+    {
+        _playerInput = transform.parent.parent.GetComponent<PlayerInput>();
+        _playerInput.actions.FindAction("Interactable").performed += HammerHammer;
+    }
+
+    private void HammerHammer(InputAction.CallbackContext pCallback)
+    {
+        foreach (var gameObjectInReach in _gameObjectsInTrigger)
+        {
+            Debug.Log("HAMMERED");
+            Transform transformGO = gameObjectInReach.transform;
+            transformGO.localScale -= new Vector3(0, transformGO.localScale.y - 0.2f, 0);
+            if (Physics.Raycast(gameObjectInReach.transform.position, Vector3.down, out var raycastHit,
+                float.PositiveInfinity))
+            {
+                gameObjectInReach.transform.position = raycastHit.point + new Vector3(0,gameObjectInReach.transform.lossyScale.y / 2,0);
+            }
+            
+            Rigidbody rigidbody = gameObjectInReach.GetComponent<Rigidbody>();
+            if (rigidbody != null)
+                rigidbody.AddForce(Vector3.up * 5.0f, ForceMode.Impulse);
+        }
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!_gameObjectsInTrigger.Contains(other.gameObject))
+        {
+            if((interactableLayers.value & (1 << other.gameObject.layer)) > 0)
+                _gameObjectsInTrigger.Add(other.gameObject);
+        }
+    }    
+    
+    private void OnTriggerExit(Collider other)
+    {
+        if(_gameObjectsInTrigger.Contains(other.gameObject)) 
+            _gameObjectsInTrigger.Remove(other.gameObject);
+    }
+}
