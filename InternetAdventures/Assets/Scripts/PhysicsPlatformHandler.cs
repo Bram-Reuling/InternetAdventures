58ea9b724cf0d09ff188f713833010e5603ba538
Maurice Johannssen <60900273+RulesOfDelta@users.noreply.github.com>
2021-05-19 15:32:10 +0200

    Physics platform can move back to init position

--- InternetAdventures/Assets/Scripts/PhysicsPlatformHandler.cs
+++ InternetAdventures/Assets/Scripts/PhysicsPlatformHandler.cs
@@ -4,17 +4,46 @@ using System.Collections.Generic;
 using DG.Tweening;
 using Unity.VisualScripting;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class PhysicsPlatformHandler : MonoBehaviour
 {
     [SerializeField] private GameObject platform1, platform2;
+    [SerializeField] private bool movePlatformsBack;
+    [SerializeField] private float movementDuration;
     private GameObject _actuatedPlatform;
     private Vector3 _endPosition = Vector3.zero;
+    private Vector3 _initialMovementVector;
 
+    private Vector3 _platform1InitPosition;
+    private Vector3 _platform2InitPosition;
+
+    private void Start()
+    {
+        //Save initial position to check difference when moving back.
+        _platform1InitPosition = platform1.transform.position;
+        _platform2InitPosition = platform2.transform.position;
+        
+        //Calculate movement vector by taking the higher platform.
+        Transform platformToUse = platform1.transform.position.y >= platform2.transform.position.y
+            ? platform1.transform
+            : platform2.transform;
+        if (Physics.Raycast(transform.TransformDirection(platformToUse.position), Vector3.down, out var hit,
+            float.PositiveInfinity))
+        {
+            _initialMovementVector = hit.point - transform.TransformDirection(platform2.transform.position);
+        }
+        else
+        {
+            Debug.LogError("Could not calculate the movement vector of the physics platform!");
+        }
+    }
+    
+    
     public void OnActuation(GameObject pPlatform, GameObject pActuator)
     {
         if (pPlatform == _actuatedPlatform) return;
-
+        
         Vector3 platformToUser = pActuator.transform.position - pPlatform.transform.position;
         if (Vector3.Dot(platformToUser.normalized, Vector3.up) < 0.2f) return;
         
@@ -37,8 +66,14 @@ public class PhysicsPlatformHandler : MonoBehaviour
 
     public void StopActuation()
     {
-        Debug.Log("Actuation stopped");
-        platform1.GetComponent<Rigidbody>().DOKill();
-        platform2.GetComponent<Rigidbody>().DOKill();
+        if (movePlatformsBack)
+            MovePlatformsBack();
+    }
+    
+    private void MovePlatformsBack()
+    {
+        platform1.GetComponent<Rigidbody>().DOMoveY(_platform1InitPosition.y, movementDuration);
+        platform2.GetComponent<Rigidbody>().DOMoveY(_platform2InitPosition.y, movementDuration);
+        _actuatedPlatform = null;
     }
 }
