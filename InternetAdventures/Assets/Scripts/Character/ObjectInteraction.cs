1cffab19d87242bfe8d3276b2a1d8f791d39fa12
Maurice Johannssen <60900273+RulesOfDelta@users.noreply.github.com>
2021-05-20 17:01:03 +0200

    Added ObjectInteraction layer dependency

--- InternetAdventures/Assets/Scripts/Character/ObjectInteraction.cs
+++ InternetAdventures/Assets/Scripts/Character/ObjectInteraction.cs
@@ -5,13 +5,18 @@ using UnityEngine;
 
 public class ObjectInteraction : MonoBehaviour
 {
-
+    //Public
     [SerializeField] private float pushForce;
+    
+    //Private
     public LayerMask interactableLayers;
 
     private void OnControllerColliderHit(ControllerColliderHit hit)
     {
-        if(hit.rigidbody != null)
-            hit.rigidbody.AddForce((hit.transform.position - transform.position) * pushForce);
+        if (hit.rigidbody != null)
+        {
+            if((interactableLayers & (1 << hit.gameObject.layer)) > 0)
+                hit.rigidbody.AddForce((hit.transform.position - transform.position) * pushForce);
+        }
     }
 }
