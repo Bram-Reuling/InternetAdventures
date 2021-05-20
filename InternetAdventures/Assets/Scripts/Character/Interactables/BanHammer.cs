74a9e78201c13801b1586fdb6a7640f2c4ad0346
Maurice Johannssen <60900273+RulesOfDelta@users.noreply.github.com>
2021-05-20 13:15:04 +0200

    Cleaned up BanHammer - attributes to Interactable

--- InternetAdventures/Assets/Scripts/Character/Interactables/BanHammer.cs
+++ InternetAdventures/Assets/Scripts/Character/Interactables/BanHammer.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
 using UnityEngine;
@@ -6,28 +5,38 @@ using UnityEngine.InputSystem;
 
 public class BanHammer : Interactable
 {
-    [SerializeField] private LayerMask interactableLayers;
-    [SerializeField] private bool shakeEffect;
+    //Public
+    [SerializeField] private bool enableScaleEffectOnObjects;
+    
+    //Private
     private PlayerInput _playerInput;
-    private List<GameObject> _gameObjectsInTrigger = new List<GameObject>();
+    private readonly List<GameObject> _gameObjectsInTrigger = new List<GameObject>();
 
     private void Start()
     {
         _playerInput = transform.parent.parent.GetComponent<PlayerInput>();
-        _playerInput.actions.FindAction("Interactable").performed += HammerHammer;
+        _playerInput.actions.FindAction("Interactable").performed += SlamHammer;
     }
 
-    private void HammerHammer(InputAction.CallbackContext pCallback)
+    private void SlamHammer(InputAction.CallbackContext pCallback)
     {
         if (!gameObject.activeSelf) return;
-        Camera.main.DOShakePosition(1, 1.5f);
-        Camera.main.DOShakeRotation(1, 1.5f);
-        
+        if (enableCameraShake)
+        {
+            Camera.main.DOShakePosition(cameraShakeDuration, cameraShakeStrength);
+            Camera.main.DOShakeRotation(cameraShakeDuration, cameraShakeStrength);   
+        }
+
         foreach (var gameObjectInReach in _gameObjectsInTrigger)
         {
-            if (!shakeEffect)
+            //IMPORTANT
+            //Todo: This line is subject to change depending on what the designers want
+            
+            if (!enableScaleEffectOnObjects)
             {
+                //Info: Scale object down.
                 gameObjectInReach.transform.localScale -= new Vector3(0, gameObjectInReach.transform.localScale.y - 0.2f, 0);
+                //Info: Send ray to ground and place object there.
                 if (Physics.Raycast(gameObjectInReach.transform.position, Vector3.down, out var raycastHit,
                     float.PositiveInfinity))
                 {
@@ -36,21 +45,26 @@ public class BanHammer : Interactable
             }
             else gameObjectInReach.transform.DOShakeScale(1, 2f);
             
+            //Add impulse upwards if there's a rigidbody.
             Rigidbody rigidbody = gameObjectInReach.GetComponent<Rigidbody>();
             if (rigidbody != null)
                 rigidbody.AddForce(Vector3.up * 5.0f, ForceMode.Impulse);
         }
     }
     
+    //Info: The purpose of this method is to cache all gameObjects that are currently in my trigger, so I can use
+    //that list when the hammer is being slammed.
     private void OnTriggerEnter(Collider other)
     {
         if (!_gameObjectsInTrigger.Contains(other.gameObject))
         {
+            //Only add game object when its in the interactable layers.
             if((interactableLayers.value & (1 << other.gameObject.layer)) > 0)
                 _gameObjectsInTrigger.Add(other.gameObject);
         }
     }    
     
+    //Info: The purpose here is to remove the previously added game objects from the list.
     private void OnTriggerExit(Collider other)
     {
         if(_gameObjectsInTrigger.Contains(other.gameObject)) 
--- InternetAdventures/Assets/Scripts/Character/Interactables/Interactable.cs
+++ InternetAdventures/Assets/Scripts/Character/Interactables/Interactable.cs
@@ -3,4 +3,8 @@ using UnityEngine;
 public abstract class Interactable : MonoBehaviour
 {
     //Holds information all interactable objects share like relative position etc.
+    [SerializeField] protected LayerMask interactableLayers;
+    [SerializeField] protected bool enableCameraShake;
+    [SerializeField] protected float cameraShakeStrength;
+    [SerializeField] protected float cameraShakeDuration;
 }
