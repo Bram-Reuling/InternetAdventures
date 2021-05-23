d7d5410f0b277675079635570fecc4e899feed60
Bram Reuling <bram.reuling@outlook.com>
2021-05-23 11:41:05 +0200

    Cleaned up Networking folder for headless unity server test

---
+++ InternetAdventures/Assets/Scripts/Networking/Server/GameServer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using UnityEngine;
+
+namespace Networking.Server
+{
+    public class GameServer : MonoBehaviour
+    {
+        private TcpListener listener;
+        
+        private void Awake()
+        {
+            try
+            {
+                Console.WriteLine("Starting server on port 55555");
+
+                listener = new TcpListener(IPAddress.Any, 55555);
+                listener.Start();
+            
+                Console.WriteLine("Started server!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        private void Start()
+        {
+            
+        }
+
+        private void Update()
+        {
+            
+        }
+    }
+}
