3412ad6f7da970f39ff8e6f790a1384fcd0d9305
Bram Reuling <bram.reuling@outlook.com>
2021-05-25 18:48:45 +0200

    Added network classes for moving platforms

---
+++ InternetAdventures/Assets/Scripts/Networking/Platforms/MovingPlatforms/NetworkMovingPlatformHandler.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Lean.Pool;
+using Mirror;
+using UnityEngine;
+
+namespace Networking.Platforms.MovingPlatforms
+{
+    public class NetworkMovingPlatformHandler : NetworkBehaviour
+    {
+        #region Variables
+
+        [SerializeField] private GameObject platform;
+        [SerializeField] private float duration;
+        [SerializeField] private float spawnRate;
+        [SerializeField] private bool useRandomSpawnIntervals;
+        [SerializeField] private bool loopMovement;
+        [SerializeField] private float minSpawnTime;
+        [SerializeField] private float maxSpawnTime;
+
+        private float _timePassed;
+        private int _maxPlatformAmount;
+        private int _currentPlatformAmount;
+        private readonly List<Transform> _platformStations = new List<Transform>();
+
+        #endregion
+
+        #region Global Functions
+
+        
+
+        #endregion
+
+        #region Client Functions
+
+        
+
+        #endregion
+
+        #region Server Functions
+
+        
+
+        #endregion
+        
+        private void Start()
+        {
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                _platformStations.Add(transform.GetChild(i).transform);
+            }
+
+            //Note: (transform.childCount * duration) / spawnRate gives the total amount of platforms that can be spawned
+            //properly.
+            _maxPlatformAmount = (int) (transform.childCount * duration / spawnRate);
+
+            SpawnPlatform();
+        }
+
+        private void Update()
+        {
+            _timePassed += Time.deltaTime;
+            if (!(_timePassed >= spawnRate)) return;
+            
+            if (loopMovement)
+                if (_currentPlatformAmount >= _maxPlatformAmount)
+                    return;
+            SpawnPlatform();
+        }
+
+        private void SpawnPlatform()
+        {
+            //Todo: Create a deep copy of the list, so individual scripts cannot mess up the list.
+            LeanPool.Spawn(platform, _platformStations.ElementAt(0).position, Quaternion.identity, transform)
+                .GetComponent<NetworkMovingPlatform>()
+                .Initialize(_platformStations, duration, loopMovement);
+            _timePassed = 0;
+            if (useRandomSpawnIntervals)
+                spawnRate = Random.Range(minSpawnTime, maxSpawnTime);
+            _currentPlatformAmount++;
+        }
+    }
+}
