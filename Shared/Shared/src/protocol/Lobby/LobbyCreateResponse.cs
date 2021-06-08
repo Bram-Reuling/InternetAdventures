cfaedae9e5073f6e58c9da8fc2b51d2cc98f411b
Bram Reuling <bram.reuling@outlook.com>
2021-06-08 09:06:30 +0200

    LobbyCreateResponse done

--- Shared/Shared/src/protocol/Lobby/LobbyCreateResponse.cs
+++ Shared/Shared/src/protocol/Lobby/LobbyCreateResponse.cs
@@ -1,15 +1,25 @@
+using Shared.model;
+
 namespace Shared.protocol.Lobby
 {
     public class LobbyCreateResponse : ISerializable
     {
+        public string RoomCode { get; set; } = "00000";
+        public ResponseCode ResponseCode { get; set; } = ResponseCode.Ok;
+        public string ResponseMessage { get; set; } = "";
+        
         public void Serialize(Packet pPacket)
         {
-            throw new System.NotImplementedException();
+            pPacket.Write(RoomCode);
+            pPacket.Write((int)ResponseCode);
+            pPacket.Write(ResponseMessage);
         }
 
         public void Deserialize(Packet pPacket)
         {
-            throw new System.NotImplementedException();
+            RoomCode = pPacket.ReadString();
+            ResponseCode = (ResponseCode) pPacket.ReadInt();
+            ResponseMessage = pPacket.ReadString();
         }
     }
 }
