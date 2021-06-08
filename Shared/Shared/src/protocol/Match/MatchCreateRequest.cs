a6803b5636fa6e3534c3956c5c52a5860605f260
Bram Reuling <bram.reuling@outlook.com>
2021-06-08 09:09:30 +0200

    MatchCreateRequest done

--- Shared/Shared/src/protocol/Match/MatchCreateRequest.cs
+++ Shared/Shared/src/protocol/Match/MatchCreateRequest.cs
@@ -1,15 +1,22 @@
+using Shared.model;
+
 namespace Shared.protocol.Match
 {
     public class MatchCreateRequest : ISerializable
     {
+        public int RequestingPlayerId { get; set; } = 0;
+        public Room Lobby { get; set; } = new Room();
+        
         public void Serialize(Packet pPacket)
         {
-            throw new System.NotImplementedException();
+            pPacket.Write(RequestingPlayerId);
+            pPacket.Write(Lobby);
         }
 
         public void Deserialize(Packet pPacket)
         {
-            throw new System.NotImplementedException();
+            RequestingPlayerId = pPacket.ReadInt();
+            Lobby = pPacket.Read<Room>();
         }
     }
 }
