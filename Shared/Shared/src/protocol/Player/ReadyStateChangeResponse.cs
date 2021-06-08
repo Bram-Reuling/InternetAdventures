61a033aa6247981820587afd8fc75aaf7e394137
Bram Reuling <bram.reuling@outlook.com>
2021-06-08 08:41:54 +0200

    ReadyStateChangeResponse done

--- Shared/Shared/src/protocol/Player/ReadyStateChangeResponse.cs
+++ Shared/Shared/src/protocol/Player/ReadyStateChangeResponse.cs
@@ -1,15 +1,25 @@
+using Shared.model;
+
 namespace Shared.protocol
 {
     public class ReadyStateChangeResponse : ISerializable
     {
+        public ReadyState NewReadyState { get; set; } = ReadyState.NotReady;
+        public ResponseCode ResponseCode { get; set; } = ResponseCode.Ok;
+        public string ResponseMessage { get; set; } = "";
+        
         public void Serialize(Packet pPacket)
         {
-            
+            pPacket.Write((int)NewReadyState);
+            pPacket.Write((int)ResponseCode);
+            pPacket.Write(ResponseMessage);
         }
 
         public void Deserialize(Packet pPacket)
         {
-            
+            NewReadyState = (ReadyState) pPacket.ReadInt();
+            ResponseCode = (ResponseCode) pPacket.ReadInt();
+            ResponseMessage = pPacket.ReadString();
         }
     }
 }
