cbf148bfd90319cd2123d0a20853d45674c630b9
Bram Reuling <bram.reuling@outlook.com>
2021-06-09 08:52:55 +0200

    Added script for command arguments

---
+++ InternetAdventures/Assets/Scripts/Networking/CommandArgs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+namespace Networking
+{
+    public class CommandArgs : MonoBehaviour
+    {
+        private static string cmdInfo = "";
+
+        private void Start()
+        {
+            string[] arguments = Environment.GetCommandLineArgs();
+
+            foreach (string argument in arguments)
+            {
+                cmdInfo += argument.ToString() + "\n";
+            }
+        }
+
+        private void OnGUI()
+        {
+            Rect r = new Rect(5, 5, 800, 500);
+            GUI.Label(r,cmdInfo);
+        }
+    }
+}
