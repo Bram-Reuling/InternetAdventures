5c4f27028e910a34b841643a765feedb803ec077
Maurice Johannssen <60900273+RulesOfDelta@users.noreply.github.com>
2021-06-11 14:07:03 +0200

    Scammer teleports
    Scammer only teleports when space is big enough, on which the character stands

--- InternetAdventures/Assets/Scripts/NPC/Behaviour_tree/Behaviour_Nodes/FollowObjectNode.cs
+++ InternetAdventures/Assets/Scripts/NPC/Behaviour_tree/Behaviour_Nodes/FollowObjectNode.cs
@@ -6,6 +6,8 @@ public class FollowObjectNode : Node
     private ScammerBlackboard _scammerBlackboard;
     private GameObject _objectToFollow;
     private float _distanceToObject;
+    private bool _characterJumped;
+    private int _placementTries;
 
     public FollowObjectNode(ScammerBlackboard pScammerBlackboard, GameObject pObjectToFollow, float pDistanceToObject)
     {
@@ -21,14 +23,23 @@ public class FollowObjectNode : Node
         if (scammerToObjectVec.magnitude > _distanceToObject)
         {
             nodeState = State.Success;
-            if (_scammerBlackboard.NavAgent.CalculatePath(
-                _objectToFollow.transform.position - scammerToObjectVec.normalized * _distanceToObject, navMeshPath))
+            if (_scammerBlackboard.NavAgent.CalculatePath(_objectToFollow.transform.position - scammerToObjectVec.normalized * _distanceToObject, navMeshPath)
+            && (navMeshPath.corners[navMeshPath.corners.Length - 1] - _objectToFollow.transform.position).magnitude < 3.0f)
             {
                 _scammerBlackboard.NavAgent.SetPath(navMeshPath);
+                _placementTries = 0;
             }
             else
             {
                 if (!_objectToFollow.GetComponent<CharacterController>().isGrounded)
+                {
+                    nodeState = State.Failure;
+                    _characterJumped = true;
+                    _placementTries = 0;
+                    return nodeState;
+                }
+
+                if (!_characterJumped)
                 {
                     nodeState = State.Failure;
                     return nodeState;
@@ -39,15 +50,19 @@ public class FollowObjectNode : Node
                 int itr = 0;
                 do
                 {
-                    targetPosition = _objectToFollow.transform.position + new Vector3(Random.Range(-4.0f, 4.0f), 
-                        _objectToFollow.transform.position.y, Random.Range(-4.0f, 4.0f));
+                    targetPosition = _objectToFollow.transform.position + new Vector3(Random.Range(-2.0f, 2.0f), 
+                        _objectToFollow.transform.position.y, Random.Range(-2.0f, 2.0f));
                     itr++;
-                } while (!NavMesh.SamplePosition(targetPosition, out navMeshHit, float.PositiveInfinity, NavMesh.AllAreas) && itr < 5);
+                } while (!NavMesh.SamplePosition(targetPosition, out navMeshHit, 3, NavMesh.AllAreas) && itr < 5);
 
-                _scammerBlackboard.NavAgent.enabled = false;
-                _scammerBlackboard.transform.position = targetPosition;
-                _scammerBlackboard.NavAgent.nextPosition = targetPosition;
-                _scammerBlackboard.NavAgent.enabled = true;
+                _placementTries++;
+                if (_placementTries == 5) _characterJumped = false;
+                if (itr < 5)
+                {
+                    _scammerBlackboard.NavAgent.enabled = false;
+                    _scammerBlackboard.transform.position = targetPosition;
+                    _scammerBlackboard.NavAgent.enabled = true;   
+                }
             }
         }
         else nodeState = State.Failure;
