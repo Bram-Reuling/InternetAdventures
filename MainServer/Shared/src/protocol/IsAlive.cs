4145416ef25586f1c7049dac80a168a0f20afad9
Bram Reuling <bram.reuling@outlook.com>
2021-06-16 09:12:14 +0200

    Added IsAlive packet

---
+++ MainServer/Shared/src/protocol/IsAlive.cs
@@ -0,0 +1,15 @@
+namespace Shared.protocol
+{
+    public class IsAlive : ISerializable
+    {
+        public void Serialize(Packet pPacket)
+        {
+            
+        }
+
+        public void Deserialize(Packet pPacket)
+        {
+            
+        }
+    }
+}
