92eff1d40947f2d9c5a88dfdba0caf10d9eb3fc9
Maurice Johannssen <60900273+RulesOfDelta@users.noreply.github.com>
2021-06-21 12:50:17 +0200

    InteractableTrigger that unlocks interactables

---
+++ InternetAdventures/Assets/Scripts/Character/Interactables/InteractableTrigger.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableTrigger : MonoBehaviour
+{
+    [SerializeField] private InteractableEnum Interactable;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag.Equals("Character"))
+        {
+            other.transform.GetChild(1).GetComponent<InteractableHandler>().UnlockInteractable(Interactable);
+        }
+    }
+}
