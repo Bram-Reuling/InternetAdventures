0edb4444f2e4b16a87dfaa0bf1ac85c8cad88144
Maurice Johannssen <60900273+RulesOfDelta@users.noreply.github.com>
2021-06-21 13:08:52 +0200

    Animation trigger can trigger multiple animations

--- InternetAdventures/Assets/Scripts/Animation/AnimationTrigger.cs
+++ InternetAdventures/Assets/Scripts/Animation/AnimationTrigger.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class AnimationTrigger : MonoBehaviour
 {
-    [SerializeField] private Animation animation;
+    [SerializeField] private Animator[] animation;
     
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag.Equals("Character"))
         {
-            animation.Play();
+            foreach (var currentAnimation in animation)
+            {
+                currentAnimation.enabled = true;
+            }
             Destroy(gameObject);
         }
     }
