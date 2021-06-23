5c22e15b35e3ab8a736489976248131f0b6c4d5f
Bram Reuling <bram.reuling@outlook.com>
2021-06-23 09:14:44 +0200

    Created scammer checkpoint

---
+++ InternetAdventures/Assets/Scripts/Networking/NPC/ScammerCheckPoint.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Mirror;
+using UnityEngine;
+
+namespace Networking.NPC
+{
+    [RequireComponent(typeof(BoxCollider))]
+    public class ScammerCheckPoint : NetworkBehaviour
+    {
+        [SerializeField] private GameObject scammer;
+        [SerializeField] private GameObject scammerSpawnPoint;
+
+        private bool alreadyUsed = false;
+
+        [ServerCallback]
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!alreadyUsed && other.CompareTag("Character"))
+            {
+                scammer.transform.position = scammerSpawnPoint.transform.position;
+                alreadyUsed = true;
+            }
+        }
+    }
+}
