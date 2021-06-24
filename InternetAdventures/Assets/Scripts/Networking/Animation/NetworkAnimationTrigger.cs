88ce4da583e2a48cabcaa99a614377da60963c9e
Maurice Johannssen <60900273+RulesOfDelta@users.noreply.github.com>
2021-06-24 13:42:11 +0200

    Remove destroy on animation trigger

--- InternetAdventures/Assets/Scripts/Networking/Animation/NetworkAnimationTrigger.cs
+++ InternetAdventures/Assets/Scripts/Networking/Animation/NetworkAnimationTrigger.cs
@@ -13,7 +13,6 @@ public class NetworkAnimationTrigger : NetworkBehaviour
             {
                 currentAnimation.enabled = true;
             }
-            Destroy(gameObject);
         }
     }
 }
